ed86c3d46e643936b4a96c8144c4ac31a589074f
Shubh4nk <62099471+5h0bh4nk@users.noreply.github.com>
2020-10-02 17:03:05 +0530

    Update userpage.php

--- userpage.php
+++ userpage.php
@@ -9,7 +9,7 @@
      <meta charset="utf-8">
      <meta name="viewport" content="width=device-width, initial-scale=1">
      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-     <link rel="stylesheet" type="text/css" href="styles.css">
+     <link rel="stylesheet" type="text/css" href="./css/styles.css">
 	 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" >
      <title>TEAM-14</title>
 </head>
